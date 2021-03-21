3717831a32d71b94ee2ec7d19e2627269475105b
Will-Rich-724 <williamtrich5@gmail.com>
2021-03-21 12:58:14 -0500

    addition of component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ParlayForm from './Components/ParlayForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <ParlayForm />
     </div>
   );
 }
